39738a900fa34229d6128ac4957fc214429944b4
piratewiz <mxkst.99@gmail.com>
2023-11-25 22:09:37 +0100

    maksym-cv

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+function filterBar(skills) {
+
+  var input = document.getElementById('filter')
+  var filter = input.value.toUpperCase()
+  var ul = document.getElementById('elements')
+  var li = ul.getElementsByTagName('li')
+
+  for (var i = 0; i < li.length; i++) {
+    var languageP = li[i].getElementsByTagName('p')[0]
+    var languageValue = languageP.textContent || languageP.innerText;
+
+    if (languageValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = 'block'
+    } else {
+      li[i].style.display = 'none'
+    };
+  };
+};
